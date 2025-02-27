ee7caa437bdb1827341ae898a3d1bbc6f475cee2
Vick-17 <vega.victor.0712@gmail.com>
2025-02-27 15:13:51 +0100

    Minor fix

--- api/src/main/java/com/projectspring/api/Services/UserService.java
+++ api/src/main/java/com/projectspring/api/Services/UserService.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.projectspring.api.Dto.UserDto;
 import com.projectspring.api.Generic.GenericService;
